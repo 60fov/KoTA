bc36c7a225f490d71867093d07330d6cde0dd98c
j$ <55032731+60fov@users.noreply.github.com>
2023-05-20 01:56:46 -0500

    created avatar component

---
+++ src/components/ui/Avatar.tsx
@@ -0,0 +1,34 @@
+import { useSession } from "next-auth/react";
+import { type ReactNode } from "react";
+import { cn } from "~/utils/fns";
+
+// TODO: use next js image
+// import Image from 'next/image';
+
+
+interface Props {
+}
+
+const Avatar = (props: Props) => {
+  const {
+    ...restProps
+  } = props;
+
+  const session = useSession()
+
+  const path = session.data?.user.image || "/mesh-gradient.png"
+
+  return (
+    <div className={cn(
+      "rounded-full overflow-clip"
+    )}>
+      <img
+        src={path}
+        alt={"user avatar"}
+        className={cn()} />
+    </div>
+  )
+}
+
+export default Avatar
+export type AvatarProps = Props
