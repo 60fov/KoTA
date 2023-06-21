7320d920a78098ddaa1b205465d20d0ddfe5bdc9
j$ <55032731+60fov@users.noreply.github.com>
2023-06-21 03:46:43 -0500

    non dup props type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -7,3 +7,6 @@ export interface CSSVariableProperties extends CSSProperties {
 type VariantList = readonly (string | number)[]
 export type VariantValue<T extends VariantList> = T[number]
 export type VariantLUT<T extends string | number> = { [K in T]: string }
+
+
+export type NonDuplicateProps<T, U> = Pick<U, Exclude<keyof U, keyof T>> & T;
