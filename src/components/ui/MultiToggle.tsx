48c5b3d7e79857eb3dcdaa750b06e3116cb3fb77
j$ <55032731+60fov@users.noreply.github.com>
2023-06-26 02:09:29 -0500

    multi toggle module conversion

--- src/components/ui/MultiToggle.tsx
+++ src/components/ui/MultiToggle.tsx
@@ -1,10 +1,13 @@
-import type { CSSProperties, FormEventHandler, ReactElement, ReactNode } from "react"
-import { useState } from "react"
+import type { FormEventHandler, ReactElement, ReactNode } from "react"
+import { useEffect, useState } from "react"
 
 import { motion } from "framer-motion"
 
-import { type CSSVariableProperties } from "~/utils/types"
 import { cn, createSafeCtx } from "~/utils/fns"
+import { type Controllable } from "./types"
+
+import ui from "./UI.module.scss"
+import styles from "./MultiToggle.module.scss"
 
 interface MultiToggleContextInterface {
   name: string
@@ -13,12 +16,9 @@ interface MultiToggleContextInterface {
 
 const [useMultiToggleContext, MultiToggleProvider] = createSafeCtx<MultiToggleContextInterface>()
 
-interface Props<T extends string> {
+interface Props<T extends string> extends Controllable<T> {
   name: string
   prompt: string
-  defaultValue?: T
-  value?: T
-  onValueChange?: (value: T) => void
   children: ReactElement<ItemProps<T>>[] | ReactElement<ItemProps<T>>
 }
 
@@ -32,28 +32,25 @@ const Base = <T extends string>(props: Props<T>) => {
     children
   } = props;
 
-  const [value, setValue] = useState<T | undefined>(valueProp ?? defaultValue)
+  const [value, setValue] = useState(valueProp ?? defaultValue)
+
+  useEffect(() => {
+    if (valueProp) setValue(valueProp)
+  }, [valueProp])
 
   const handleChange: FormEventHandler<HTMLFieldSetElement> = (e) => {
     const input = e.target as HTMLInputElement
     const newValue = input.value as T
-    // TODO: calling both could be two render
     setValue(newValue)
     if (onValueChange) onValueChange(newValue)
   }
 
   return (
     <div
-      style={{ "--spacing": "2px" } as CSSVariableProperties}
-      className={cn(
-        // 🙏
-        // "focus-visiblie-within:outline outline-access outline-1 outline-offset-2",
-        "transition-all duration-75",
-        "p-v h-8 box-content flex",
-        "bg-back-alt/75 border-[0.5px] border-front/10 rounded-md"
-      )}>
-      <fieldset onChange={handleChange} className="flex">
-        <legend className="absolute [clip:rect(0,0,0,0)]">{prompt}</legend>
+      tabIndex={1}
+      className={cn(ui.outline, styles.base)}>
+      <fieldset onChange={handleChange}>
+        <legend data-legend>{prompt}</legend>
         <MultiToggleProvider value={{ name, value }}>
           {children}
         </MultiToggleProvider>
@@ -65,18 +62,15 @@ const Base = <T extends string>(props: Props<T>) => {
 interface ItemProps<T extends string> {
   value: T
   children?: ReactNode
-  style?: CSSProperties
-
 }
 
 
-export const Item = <T extends string>({ value, children = value, style }: ItemProps<T>) => {
+export const Item = <T extends string>({ value, children = value }: ItemProps<T>) => {
   const { name: toggleName, value: toggleValue } = useMultiToggleContext()
 
   return (
-    <div className="relative flex">
+    <div data-mutlitoggle-item>
       <input
-        className="peer appearance-none absolute"
         type="radio"
         // @typescript-eslint/no-unsafe-assignment
         name={toggleName}
@@ -85,36 +79,17 @@ export const Item = <T extends string>({ value, children = value, style }: ItemP
         checked={toggleValue === value}
         readOnly={true}
       />
-      <label
-        className={cn(
-          "relative",
-          "flex w-8 h-full items-center justify-center cursor-pointer rounded-md",
-          "text-sm text-front/50 border-front/5 transition-[color] outline-access outline-1 outline-offset-2",
-          "hover:text-front/80",
-          "peer-checked:text-front",
-          "peer-focus-visible:outline"
-        )}
-        htmlFor={value}
-        style={style}
-      >
-        <div className="relative w-8 h-8 flex items-center justify-center">
-          <span className="z-10">
-            {children}
-          </span>
-          {toggleValue === value &&
-            <motion.div
-              // @typescript-eslint/restrict-template-expressions
-              layoutId={`${toggleName}-value-bg`}
-              transition={{
-                stiffness: 120
-              }}
-              className={cn(
-                "absolute w-full h-full",
-                "text-front rounded bg-back border-front/5 border-[0.5px] shadow-md",
-              )}
-            />
-          }
+      <label htmlFor={value}>
+        <div data-children>
+          {children}
         </div>
+        {toggleValue === value &&
+          <motion.div
+            data-background
+            layoutId={`${toggleName}-value-bg`}
+            transition={{ stiffness: 120 }}
+          />
+        }
       </label>
     </div>
   )
